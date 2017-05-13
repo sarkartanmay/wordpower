1a5b3d2033a8cd54b5e790473e7885f76b05f9dd
Tanmay Sarkar <tanmayit08@gmail.com>
2017-05-13 10:04:34 +0530

    Home Tab
    Updated with Home Tab

--- chrome_extension/wh.js
+++ chrome_extension/wh.js
@@ -3,7 +3,7 @@
 	try{
 		chrome.storage.local.get('p1', function(result){
 		old_value = parseFloat(result.p1);
-		document.getElementById('item_reflect').textContent = "Your Score : "+old_value;		
+		document.getElementById('item_reflect').innerHTML = "Your Score : "+old_value;		
 	  });
 	}catch(e){}
 
@@ -17,6 +17,6 @@
 		for(i=0;i<data.length;i++){
 			msg = msg + "<b>"+data[i].search_word + "</b> - Synonyms : "+ data[i].synonyms +"<br/>";
 		}
-		document.getElementById('status').innerHTML = "<b>You have searched : </b><br/><br/>"+msg;
+		document.getElementById('status').innerHTML = "<br/><b>You have searched : </b><br/><br/>"+msg;
 	  });
 	}catch(e){}
